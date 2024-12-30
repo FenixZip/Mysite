3e6b18a935651126ea0a9ed95761ab5e6d762e0c
Oleg Sorokin <fenix15@inbox.ru>
2024-12-30 15:24:55 +0300

    add class Product

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    class Product
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Название продукта {self.name} => ${self.price}'
+
